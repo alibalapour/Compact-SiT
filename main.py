22183b8cbfc89fa49b1128e0e592ddaa7e079c26
Ali Balapour <42287060+alibalapour@users.noreply.github.com>
2021-11-14 23:37:41 +0330

    Update main.py

--- main.py
+++ main.py
@@ -376,7 +376,7 @@ def main(args):
         checkpoint = torch.load(args.resume, map_location='cpu')
         model_without_ddp.load_state_dict(checkpoint['model'])
         if not args.eval and 'optimizer' in checkpoint and 'lr_scheduler' in checkpoint and 'epoch' in checkpoint:
-            if not args.reset:
+            if not args.reset_opt:
                 optimizer.load_state_dict(checkpoint['optimizer'])
                 lr_scheduler.load_state_dict(checkpoint['lr_scheduler'])
             args.start_epoch = checkpoint['epoch'] + 1
