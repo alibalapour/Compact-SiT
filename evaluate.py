d9064308aaa3c073db0cf0bd92116f9708c9d687
Ali Balapour <42287060+alibalapour@users.noreply.github.com>
2021-11-28 11:51:35 +0330

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -115,45 +115,61 @@ def main(args):
     model.load_state_dict(checkpoint['model'])
     model.to(args.device)
             
-            
-    correct_test = 0
-    total_test = 0
-    predicted_output = []
-    predicted_probs = []
-    targets = []
-    with torch.no_grad():
-        for data in dataloader_test:
-            tensor = data[0]
-            tensor = tensor.to(args.device)
-            label = data[1]
-            targets += label.tolist()
-            label = label.to(args.device)
-            output_1, output_2 = model(tensor)
-            output = (output_1 + output_2) / 2
-            predicted = torch.argmax(output, 1)
-            predicted_prob = torch.softmax(output, 1)
-            predicted_probs += predicted_prob.tolist()
-            predicted_output += predicted.tolist()
-            total_test += tensor.size(0)
-            correct_test += (predicted == label).sum().item()
-    print('Accuracy on Test Data :', 100 * (correct_test / total_test), '%')
+    if not args.feature_extractor:     # normal analysis       
+        correct_test = 0
+        total_test = 0
+        predicted_output = []
+        predicted_probs = []
+        targets = []
+        with torch.no_grad():
+            for data in dataloader_test:
+                tensor = data[0]
+                tensor = tensor.to(args.device)
+                label = data[1]
+                targets += label.tolist()
+                label = label.to(args.device)
+                output_1, output_2 = model(tensor)
+                output = (output_1 + output_2) / 2
+                predicted = torch.argmax(output, 1)
+                predicted_prob = torch.softmax(output, 1)
+                predicted_probs += predicted_prob.tolist()
+                predicted_output += predicted.tolist()
+                total_test += tensor.size(0)
+                correct_test += (predicted == label).sum().item()
+        print('Accuracy on Test Data :', 100 * (correct_test / total_test), '%')
 
-    predicted_output = np.array(predicted_output)
-    predicted_probs = np.array(predicted_probs)
-    targets = np.array(targets)
+        predicted_output = np.array(predicted_output)
+        predicted_probs = np.array(predicted_probs)
+        targets = np.array(targets)
 
-    if args.nb_classes == 2:  # binary classification evaluation
-        print(classification_report(targets, predicted_output))
-        print("kappa score :", cohen_kappa_score(targets, predicted_output))
-        bc_evaluation = BC_Evaluation()
-        print(bc_evaluation.evaluate(targets, predicted_output))
-    else:  # multi classification evaluation
-        print(classification_report(targets, predicted_output))
-        print("kappa score :", cohen_kappa_score(targets, predicted_output))
-        print("auc macro :", roc_auc_score(targets, predicted_probs, average="macro", multi_class="ovr"))
-        print("macro f1 :", f1_score(targets, predicted_output, average="macro"))
-        print("weighted f1 :", f1_score(targets, predicted_output, average="weighted"))
-
+        if args.nb_classes == 2:  # binary classification evaluation
+            print(classification_report(targets, predicted_output))
+            print("kappa score :", cohen_kappa_score(targets, predicted_output))
+            bc_evaluation = BC_Evaluation()
+            print(bc_evaluation.evaluate(targets, predicted_output))
+        else:  # multi classification evaluation
+            print(classification_report(targets, predicted_output))
+            print("kappa score :", cohen_kappa_score(targets, predicted_output))
+            print("auc macro :", roc_auc_score(targets, predicted_probs, average="macro", multi_class="ovr"))
+            print("macro f1 :", f1_score(targets, predicted_output, average="macro"))
+            print("weighted f1 :", f1_score(targets, predicted_output, average="weighted"))
+    
+    else:
+        outputs = []
+        labels = []
+        with torch.no_grad():
+            for data in dataloader_test:
+                tensor = data[0]
+                tensor = tensor.to(args.device)
+                label = data[1]
+                targets += label.tolist()
+                label = label.to(args.device)
+                output = model(tensor)
+                outputs += output.tolist()
+                predicted = torch.argmax(output, 1)
+                predicted_output += predicted.tolist()
+        print(outputs)
+        
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('SiT evaluation script', parents=[get_args_parser()])
