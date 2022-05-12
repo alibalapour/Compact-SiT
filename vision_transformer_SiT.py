5f05f998d74c3e78b7eca8ae46456129592a40d9
Ali Balapour <42287060+alibalapour@users.noreply.github.com>
2022-05-12 19:53:27 +0430

    Update vision_transformer_SiT.py

--- vision_transformer_SiT.py
+++ vision_transformer_SiT.py
@@ -371,9 +371,9 @@ def checkpoint_filter_fn(state_dict, model):
 
 
 @register_model
-def SiT_compact_patch16_224(pretrained=False, patch_size=16, **kwargs):
+def SiT_compact_patch16_224(pretrained=False, **kwargs):
     model = VisionTransformer_SiT(
-        patch_size=patch_size, embed_dim=192, depth=14, num_heads=6, mlp_ratio=3, qkv_bias=True,
+        embed_dim=192, depth=14, num_heads=6, mlp_ratio=3, qkv_bias=True,
         norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
     model.default_cfg = _cfg()
     return model
